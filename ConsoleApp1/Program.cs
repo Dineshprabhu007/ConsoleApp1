77b61c817adb965426c933325f911dcfb894b410
Dinesh.Nanjappan <Dinesh.Nanjappan@SGT242037217315>
2024-04-15 11:16:21 +0530

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FirstProgram
+{
+    class Sample
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("H");
+        }
+    }
+}
